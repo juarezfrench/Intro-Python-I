fc7465d22c1247a38ca077bd53f9cd5bd9961e7a
juarezfrench <markedfrench@gmail.com>
2020-07-07 17:41:02 -0500

    Pages 9 and 10

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,10 +2,22 @@
 
 # YOUR CODE HERE
 
+x = 22
+
+def even():
+    if x%2 == 0:  
+        print("Even!")
+print(even())        
+       
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
+def key_even():
+    if num%2 == 0:  
+        print("Even!")
+    else:
+        print("Odd!")
+print(key_even())
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
